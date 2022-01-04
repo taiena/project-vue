261e899509eb4df9637fe27ee75d3a1dba0054a0
Anna Domaskina <taienametztli@mail.ru>
2022-01-04 18:51:53 +0300

    adding & removing elements

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,72 @@
 <template>
   <body>
     <div>
-      <label for="wallet">Тикер</label>
+      <label for="wallet">Тикер {{ ticker }}</label>
       <input
+        v-model="ticker"
+        v-on:keydown.enter="add"
         type="text"
         name="wallet"
         id="wallet"
         placeholder="Например DOGE"
       />
-      <button type="button">Добавить</button>
-    </div>
-    <hr />
-
-    <div>
-      <div>
-        <dt>WTF - USD</dt>
-        <dd>1.11</dd>
-      </div>
-      <button>Удалить</button>
+      <button
+        @click="add"
+        type="button"
+      >Добавить</button>
     </div>
 
-    <div>
-      <div>
-        <dt>WTF - USD</dt>
-        <dd>1.11</dd>
-      </div>
-      <button>Удалить</button>
-    </div>
+    <template v-if="tickers.length">
+      <hr />
 
-    <div>
-      <div>
-        <dt>WTF - USD</dt>
-        <dd>1.11</dd>
+      <div
+        v-for="t in tickers"
+        :key="t"
+      >
+        <div>
+          <div>{{ t.name }} - USD</div>
+          <div>{{ t.price }}</div>
+        </div>
+        <button @click="handleDelete(t)">Удалить</button>
       </div>
-      <button>Удалить</button>
-    </div>
 
-    <div>
-      <div>
-        <dt>WTF - USD</dt>
-        <dd>1.11</dd>
-      </div>
-      <button>Удалить</button>
-    </div>
-    
+      <hr />
+    </template>
+
+
   </body>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
+  data() {
+    return {
+      ticker: 'default',
+      tickers: [
+        { name: 'DEMO1', price: '0' },
+        { name: 'DEMO2', price: '0' },
+        { name: 'DEMO3', price: '0' }
+      ]
+    }
+  },
+
+  methods: {
+    add() {
+      const newTicker = {
+        name: this.ticker,
+        price: "0"
+      }
+
+      this.tickers.push(newTicker)
+      this.ticker=""
+    },
+
+    handleDelete(tickerToRemove) {
+      this.tickers = this.tickers.filter(t => t !== tickerToRemove)
+    }
+  }
 }
 </script>
 
