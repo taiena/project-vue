af853863bc47a5ff1b4ac3b6405a7354736024b8
Anna Domaskina <taienametztli@mail.ru>
2022-01-13 18:08:59 +0300

    fixed formatPrice

--- src/App.vue
+++ src/App.vue
@@ -179,6 +179,9 @@ export default {
     },
 
     formatPrice(price) {
+      if (price === "-") {
+        return price
+      }
       return price > 1 ? price.toFixed(2) : price.toPrecision(2)
     },
 
