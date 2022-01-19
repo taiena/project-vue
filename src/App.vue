9462c880c0907020e85846c67b9f0f16adbe7734
Anna Domaskina <taienametztli@mail.ru>
2022-01-19 13:58:32 +0300

    fixed missed graph

--- src/App.vue
+++ src/App.vue
@@ -186,7 +186,12 @@ export default {
     updateTicker(tickerName, price) {
       this.tickers
        .filter(t => t.name === tickerName)
-       .forEach(t => {t.price = price})
+       .forEach(t => {
+         if (t === this.selectedTicker) {
+           this.graph.push(price)
+         }
+         t.price = price
+        })
     },
 
     formatPrice(price) {
